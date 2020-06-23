f9fc7b6a5e6ac9d3316b6b782730fd21a47b36f9
Mohamedosamaeltayeb <mohamedosamaeltayeb@gmail.com>
2020-06-23 23:38:25 +0200

    Complete project

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_19_083256) do
+ActiveRecord::Schema.define(version: 2020_06_23_141141) do
 
   create_table "tweets", force: :cascade do |t|
     t.text "tweet"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id"
   end
 
   create_table "users", force: :cascade do |t|
